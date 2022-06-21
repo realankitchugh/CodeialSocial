a8a11ef41a1316804bb50c939b60741ceb35f0ef
Ankit Chugh <ankitchugh06@gmail.com>
2022-06-21 23:51:58 +0530

    Create sign out, sign in and sign up links in header of each page depending on whether user is signed in or not

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -54,3 +54,11 @@ module.exports.create = function(req, res){
 module.exports.createSession = function(req, res){
     return res.redirect('/users/profile');
 }
+
+module.exports.destroySession = function(req, res, next){
+    req.logout(function(err){
+        if(err){return next(err);}
+        return res.redirect('/');
+    });
+    // return res.redirect('/');
+}
